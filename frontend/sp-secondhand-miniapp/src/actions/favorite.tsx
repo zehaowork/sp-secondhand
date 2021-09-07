55daead637edb189d0dabd8fab5fcac62d5c9dd0
zehaowork <58049745+zehaowork@users.noreply.github.com>
2021-09-07 21:57:38 +0100

    doc:优化redux action部分的comment

--- frontend/sp-secondhand-miniapp/src/actions/favorite.tsx
+++ frontend/sp-secondhand-miniapp/src/actions/favorite.tsx
@@ -1,13 +1,21 @@
 import API from "../../utils/API"
 import { GET_FAVORITE_REQUEST, GET_FAVORITE_FAIL, GET_FAVORITE_SUCCESS } from "../constants/favorite"
 
-
+/**
+ * 
+ * @returns 返回action类型，reducer根据类型更改state
+ */
 const FETCH_FAVORITE_REQUEST = ()=>{
     return {
         type:GET_FAVORITE_REQUEST,
     }
 }
 
+/**
+ * 
+ * @param payload 请求返回的数据对象
+ * @returns 返回数据对象和action类型给reducer
+ */
 const FETCH_FAVORITE_SUCCESS = (payload)=>{
     return {
         type:GET_FAVORITE_SUCCESS,
@@ -15,6 +23,11 @@ const FETCH_FAVORITE_SUCCESS = (payload)=>{
     }
 }
 
+/**
+ * 
+ * @param payload 请求失败返回的错误信息对象
+ * @returns 返回错误信息的对象和action类型给reducer
+ */
 const FETCH_FAVORITE_FAIL = (payload)=>{
     return {
         type:GET_FAVORITE_FAIL,
@@ -22,7 +35,11 @@ const FETCH_FAVORITE_FAIL = (payload)=>{
     }
 }
 
-//用户界面用来dispatch的，根据不同的状态来navigate到不同的 action
+/**
+ * 
+ * @param userId 用户ID
+ * @returns 根据不同的情况返回相应的action类型
+ */
 export const getFavoriteList = (userId:string)=>{
     return function(dispatch) {
         dispatch(FETCH_FAVORITE_REQUEST());
